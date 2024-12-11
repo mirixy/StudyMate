98047d0af2137d596d26f7057f10b6f2e3fe3b96
mirixy <miriam.lucy@outlook.com>
2024-12-11 23:08:52 +0100

    Working again! MVP

--- models/db.py
+++ models/db.py
@@ -13,8 +13,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     todos = db.relationship('ToDo', backref='user', lazy=True)
-    subjects = db.relationship('Subject', backref='user', lazy=True)
-    grades = db.relationship('Grade', backref='user', lazy=True)
 
     @property
     def is_authenticated(self):
@@ -39,28 +37,3 @@ class ToDo(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-class Subject(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    is_abitur_subject = db.Column(db.Boolean, default=False)  # Indicates if this is an Abitur subject
-    grades = db.relationship('Grade', backref='subject', lazy=True, cascade="all, delete-orphan")
-
-    def __repr__(self):
-        return f'<Subject {self.name}>'
-
-class Grade(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    semester = db.Column(db.Integer, nullable=False)  # 1-4 for two years
-    final_grade = db.Column(db.Float, nullable=False)  # Final grade for the semester
-    is_abitur = db.Column(db.Boolean, default=False)  # Indicates if this is an Abitur grade
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-    __table_args__ = (
-        db.UniqueConstraint('subject_id', 'semester', name='unique_subject_semester'),
-    )
-
-    def __repr__(self):
-        return f'<Grade {self.final_grade} in {self.subject.name}>'
